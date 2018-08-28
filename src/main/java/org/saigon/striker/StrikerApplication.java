22c16464414fe37d847c7e85d2c059d36a5ddefa
Matej Kupciha <matej.kupciha@email.com>
2018-08-28 23:23:25 +0700

    Replace tab indentation with spaces

--- src/main/java/org/saigon/striker/StrikerApplication.java
+++ src/main/java/org/saigon/striker/StrikerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StrikerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StrikerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StrikerApplication.class, args);
+    }
 }
