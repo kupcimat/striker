25646b1974619c238874b1a417c8c49c3c042ae2
Matej Kupciha <matej.kupciha@gooddata.com>
2018-11-27 11:07:27 +0700

    Replace deprecated gradle features

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
+group = "org.saigon"
+version = "0.0.1-SNAPSHOT"
+
 plugins {
     java
     id("org.springframework.boot") version "2.1.0.RELEASE"
-    id("io.spring.dependency-management") version "1.0.6.RELEASE"
 }
 
-group = "org.saigon"
-version = "0.0.1-SNAPSHOT"
+apply(plugin = "io.spring.dependency-management")
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -25,16 +26,16 @@ repositories {
 }
 
 dependencies {
-    compile("org.apache.commons:commons-lang3")
-    compile("org.springframework.boot:spring-boot-starter-actuator")
-    compile("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-    compile("org.springframework.boot:spring-boot-starter-security")
-    compile("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.apache.commons:commons-lang3")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-    testCompile("org.springframework.boot:spring-boot-starter-test")
-    testCompile("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 
-    testRuntime("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+    testRuntimeOnly("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
 }
 
 tasks {
